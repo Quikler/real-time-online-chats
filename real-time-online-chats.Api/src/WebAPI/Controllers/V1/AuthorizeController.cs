54ceed113d595862c43ae25c93213270081da9a2
Quikler <romuchbinyk228@gmail.com>
2025-06-17 00:14:39 +0300

    refactor(base-controller): Refactor to AuthorizeController.

--- real-time-online-chats.Api/src/WebAPI/Controllers/V1/AuthorizeController.cs
+++ real-time-online-chats.Api/src/WebAPI/Controllers/V1/AuthorizeController.cs
@@ -3,7 +3,7 @@ using real_time_online_chats.Server.Extensions;
 
 namespace real_time_online_chats.Server.Controllers.V1;
 
-public class BaseController : ControllerBase
+public class AuthorizeController : ControllerBase
 {
     protected Guid UserId => HttpContext.GetUserId() ?? throw new Exception("User is not authenticated.");
 }
