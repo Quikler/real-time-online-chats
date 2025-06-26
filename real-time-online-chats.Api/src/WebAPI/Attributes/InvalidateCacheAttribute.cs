4a51f034620a0c6f5f0c0134370287c3d11b075b
Quikler <romuchbinyk228@gmail.com>
2025-06-26 19:08:13 +0300

    chore(api): Add ctor with templates param in InvalidateCache attribute.

--- real-time-online-chats.Api/src/WebAPI/Attributes/InvalidateCacheAttribute.cs
+++ real-time-online-chats.Api/src/WebAPI/Attributes/InvalidateCacheAttribute.cs
@@ -4,52 +4,44 @@ using real_time_online_chats.Server.Services.Cache;
 
 namespace real_time_online_chats.Server.Attributes;
 
-public class InvalidateCacheAttribute(string template, int timeToLiveSeconds) : Attribute, IAsyncActionFilter
+[AttributeUsage(AttributeTargets.Method)]
+public class InvalidateCacheAttribute(string[] templates, int timeToLiveSeconds) : Attribute, IAsyncActionFilter
 {
+    public InvalidateCacheAttribute(string template, int timeToLiveSeconds) : this([template], timeToLiveSeconds) { }
+
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
         var executionContext = await next();
         var cacheService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
 
-        foreach (var (key, value) in context.ActionArguments)
+        foreach (var template in templates)
         {
-            template = template.Replace($"{{{key}}}", value?.ToString());
+            var currentTemplate = template;
+            foreach (var (key, value) in context.ActionArguments)
+            {
+                currentTemplate = currentTemplate.Replace($"{{{key}}}", value?.ToString());
+            }
+
+            Console.WriteLine($"[InvalidateCache]: {currentTemplate}");
+
+            switch (executionContext.Result)
+            {
+                // POST
+                case CreatedAtActionResult created when created.Value is not null:
+                    await cacheService.CacheResponseAsync(currentTemplate, created.Value, TimeSpan.FromSeconds(timeToLiveSeconds));
+                    break;
+
+                // PUT
+                // PATCH
+                case OkObjectResult ok when ok.Value is not null:
+                    await cacheService.CacheResponseAsync(currentTemplate, ok.Value, TimeSpan.FromSeconds(timeToLiveSeconds));
+                    break;
+
+                // DELETE
+                case NoContentResult:
+                    await cacheService.RemoveCachedResponseAsync(currentTemplate);
+                    break;
+            }
         }
-
-        switch (executionContext.Result)
-        {
-            // POST
-            case CreatedAtActionResult created when created.Value is not null:
-                await cacheService.CacheResponseAsync(template, created.Value, TimeSpan.FromSeconds(timeToLiveSeconds));
-                break;
-
-            // PUT
-            // PATCH
-            case OkObjectResult ok when ok.Value is not null:
-                await cacheService.CacheResponseAsync(template, ok.Value, TimeSpan.FromSeconds(timeToLiveSeconds));
-                break;
-
-            // DELETE
-            case NoContentResult noContent:
-                await cacheService.RemoveCachedResponseAsync(template);
-                break;
-        }
-
-        //switch (context.HttpContext.Request.Method)
-        //{
-            //case "POST":
-                //if (executionContext.Result is not CreatedAtActionResult created || created.Value is null) return;
-                //await cacheService.CacheResponseAsync(template, created.Value, TimeSpan.FromSeconds(timeToLiveSeconds));
-                //break;
-            //case "PUT":
-            //case "PATCH":
-                //if (executionContext.Result is not OkObjectResult ok || ok.Value is null) return;
-                //await cacheService.CacheResponseAsync(template, ok.Value, TimeSpan.FromSeconds(timeToLiveSeconds));
-                //break;
-            //case "DELETE":
-                //if (executionContext.Result is not NoContentResult noContent) return;
-                //await cacheService.RemoveCachedResponseAsync(template);
-                //break;
-        //}
     }
 }
